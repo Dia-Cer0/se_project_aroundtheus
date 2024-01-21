a409b45155e30c22e0182614f7b5c8d2cdd69eda
ehutch <eric.chandler.hutcheson@gmail.com>
2024-01-20 20:01:49 -0600

    Project 4 Stage 1

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let initialCards=[
+  destination1{
+    name:"Yosemite Valley",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+
+
+},
+destination2{
+  name:"Lake Louise",
+  link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+},
+destination3{
+  name:"Bald Mountains",
+  link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+},
+destination4{
+  name:"Latemar",
+  link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+},
+destination5{
+  name:"Vanoise National Park",
+  link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+},
+destination6{
+  name:"Lago di Braies",
+  link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+},
+];
