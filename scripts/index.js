479c9e134e94df64210cbbce69407555b8a3c1a3
ehutch <eric.chandler.hutcheson@gmail.com>
2024-02-02 19:47:48 -0600

    added element template to add elements via javascript

--- scripts/index.js
+++ scripts/index.js
@@ -49,6 +49,67 @@ let profileDescriptionInput = modal.querySelector('.modal__profile-description')
 
 let saveProfileButton = modal.querySelector('.modal__save-button');
 
+let cardTemplate = document.querySelector("#card").content;
+let cardElement = cardTemplate.querySelector(".destinations__card").cloneNode(true);
+let cardContainer = document.querySelector(".destinations.page__section");
+
+let data = [ yosemite = {
+  link:"./images/yosemite.jpg",
+  name: {
+   image:"photo of yosemite valley",
+   card_caption:"Yosemite Valley"
+  }
+},
+
+lakeLouise = {
+  link:"./images/lake-louise.jpg",
+  name: {
+   image:"photo of lake louise",
+   card_caption:"Lake Louise"
+  }
+},
+baldMountains = {
+  link:"./images/bald-mountains.jpg",
+  name: {
+   image:"photo of the bald mountains",
+   card_caption:"Bald Mountains"
+  }
+},
+latemar = {
+  link:"./images/latemar.jpg",
+  name: {
+   image:"photo of latemar",
+   card_caption:"Latemar"
+  }
+},
+vanoise = {
+  link:"./images/vanoise.jpg",
+  name: {
+   image:"photo of 'vanoise park'",
+   card_caption:"Vanoise National Park"
+  }
+},
+lago = {
+  link:"./images/lago.jpg",
+  name: {
+   image:"photo of lago di braies",
+   card_caption:"Lago di Braies"
+  }
+},
+]
+
+
+function getCardElement(data){
+  cardElement.querySelector(".destinations__card-image").src=data.link;
+  cardElement.querySelector(".destinations__card-image").alt=data.name.image;
+  cardElement.querySelector(".destinations__caption-text").textContent=data.name.card_caption;
+  return cardElement;
+}
+
+for(let n=0; n<6;n++){
+  cardContainer.append(getCardElement(data[n]).cloneNode(true));
+}
+
 editProfileButton.addEventListener("click",function(e){
   modal.classList.toggle('modal_opened');
   profileNameInput.value=currentProfileName.textContent;
